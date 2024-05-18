9483870f0bb009f1235189b8bbd07b7e79f4a7fa
Veljko Marinković <marinkovicveljko12@gmail.com>
2024-05-19 01:48:43 +0200

    started working on mobile view

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,49 +1,92 @@
-/* eslint-disable no-unused-vars */
-import React, { useState, useEffect, useCallback } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from "react-router-dom";
 import './Navbar.css';
 import logo from '../../assets/logo.png';
-import { Link } from 'react-scroll';
+import menuIcon from "../../assets/menu-icon.png";
 
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const location = useLocation();
     const navigate = useNavigate();
+
+    const [activeItem, setActiveItem] = useState(location.pathname.replace("/", "") || "");
+
+    const toggleMenu = () => {
+        setMobileMenu(prevState => !prevState);
+    };
 
     useEffect(() => {
         const handleScroll = () => {
-            const scrollTop = window.pageYOffset;
-            setIsScrolled(scrollTop > 0);
+            if (viewportWidth > 768) {
+                const scrollTop = window.pageYOffset;
+                setIsScrolled(scrollTop > 0);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        const handleResize = () => {
+            setViewportWidth(window.innerWidth);
         };
 
         window.addEventListener('scroll', handleScroll);
+        window.addEventListener('resize', handleResize);
 
         return () => {
             window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('resize', handleResize);
         };
-    }, []);
+    }, [viewportWidth]);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     const handleClick = (putanja) => {
+        setActiveItem(putanja);
         navigate(`/${putanja}`);
     };
 
     useEffect(() => {
-        // Reset scroll position to top on navigation
         window.scrollTo(0, 0);
-    }, [handleClick]);
+    }, []);
 
     return (
         <nav className={`container ${isScrolled ? 'scrolled' : ''}`}>
-            <a href="/"> <img src={logo} alt="logo img" className='logo' /></a>
-            <ul>
-                <li onClick={() => handleClick(" ")}>Početna</li>
-                <li onClick={() => handleClick("jelovnik")}>Jelovnik</li>
-                <li onClick={() => handleClick("o-nama")}>O nama </li>
-                <li onClick={() => handleClick("galerija")}>Galerija</li>
-
-                <li onClick={() => handleClick("contact")}>Kontakt</li>
+            <a href="/" className="desktop-logo"> <img src={logo} alt="logo img" className='logo' /></a>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+                <li
+                    className={activeItem === "" ? "active" : ""}
+                    onClick={() => handleClick("")}
+                >
+                    Početna
+                </li>
+                <li
+                    className={activeItem === "jelovnik" ? "active" : ""}
+                    onClick={() => handleClick("jelovnik")}
+                >
+                    Jelovnik
+                </li>
+                <li
+                    className={activeItem === "o-nama" ? "active" : ""}
+                    onClick={() => handleClick("o-nama")}
+                >
+                    O nama
+                </li>
+                <li
+                    className={activeItem === "galerija" ? "active" : ""}
+                    onClick={() => handleClick("galerija")}
+                >
+                    Galerija
+                </li>
+                <li
+                    className={activeItem === "contact" ? "active" : ""}
+                    onClick={() => handleClick("contact")}
+                >
+                    Kontakt
+                </li>
+                <a href="/" className="mobile-logo"><img src={logo} alt="logo img" className='logo' /></a>
             </ul>
-        </nav >
+            <img src={menuIcon} alt="menu icon" className='menu-icon' onClick={toggleMenu} />
+        </nav>
     );
 };
 
